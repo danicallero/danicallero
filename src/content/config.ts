52107ea76453451c505d4723ac7ffc2f42d5662f
Daniel Callero <hola@danicallero.es>
2026-07-01 17:00:55 +0200

    feat: add gallery support to blog posts and include HackUDC 2026 image

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,15 @@ const blog = defineCollection({
     date: z.date(),
     tags: z.array(z.string()).optional().default([]),
     draft: z.boolean().optional().default(false),
+    gallery: z
+      .array(
+        z.object({
+          type: z.enum(['image', 'video']),
+          src: z.string(),
+          caption: z.string().optional(),
+        })
+      )
+      .optional(),
   }),
 });
 
