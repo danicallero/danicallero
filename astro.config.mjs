e209aec6aa97c48f502eea8ce8957dc4eb1fb67a
Daniel Callero <hola@danicallero.es>
2026-07-01 16:23:43 +0200

    feat: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.danicallero.es',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx(),
+    sitemap(),
   ],
   output: 'static',
 });
